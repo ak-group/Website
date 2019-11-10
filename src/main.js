d5abfbffc1a454a7449aba489455cd3ed96e4406
Alexander Kobelev <ak.mail.ru@gmail.com>
2019-11-10 16:03:25 +0300

    Simplify code

--- src/main.js
+++ src/main.js
@@ -3,12 +3,8 @@ import App from './App.vue'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import VueApexCharts from 'vue-apexcharts'
 
 Vue.use(BootstrapVue);
-Vue.use(VueApexCharts);
-Vue.component('apexchart', VueApexCharts);
-
 Vue.config.productionTip = false;
 
 new Vue({
